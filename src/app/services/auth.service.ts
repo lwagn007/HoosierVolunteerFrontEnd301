ce182108b75a1031a02346375a1a5e6fbd322d9f
Lawrence Wagner <lawrencewagner77@gmail.con>
2018-09-18 11:09:16 -0400

    login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { Token } from '../models/Token';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Subject } from 'rxjs'
 
 const Api_Url = 'http://localhost:57751';
 
@@ -11,6 +12,8 @@ const Api_Url = 'http://localhost:57751';
   providedIn: 'root'
 })
 export class AuthService {
+  userInfo: Token;
+  isLoggedIn = new Subject<boolean>();
 
   constructor(private _http: HttpClient, private _router: Router) { }
 
@@ -21,15 +24,26 @@ export class AuthService {
   login(loginInfo){
     const str = 
     `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
-    return this._http.post(`${Api_Url}/token`, str).subscribe( (token: Token) => { localStorage.setItem('id_token', token.access_token); this._router.navigate(['/']);
+    
+    return this._http.post(`${Api_Url}/token`, str).subscribe( (token: Token) => { 
+      this.userInfo = token; localStorage.setItem('id_token', token.access_token); this._router.navigate(['/']);
     });
   }
 
   currentUser(): Observable<Object> {
     if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
 
-    const authHeader = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('id_token')}`);
+    return this._http.get(`${Api_Url}/api/Account/UserInfo`, { headers: this.setHeader() });
+  }
 
-    return this._http.get(`${Api_Url}/api/Account/UserInfo`, { headers: authHeader });
+  logout(): Observable<Object> {
+    localStorage.clear();
+    this.isLoggedIn.next(false);
+
+    return this._http.post(`${Api_Url}/api/Account/Logout`, { headers: this.setHeader() } );
+  }
+
+  private setHeader(): HttpHeaders {
+    return new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('id_token')}`);
   }
 }
