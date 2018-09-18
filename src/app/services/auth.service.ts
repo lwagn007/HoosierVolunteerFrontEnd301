f1741b1464ac99ef975f500305ed40ed15f26735
Lawrence Wagner <lawrencewagner77@gmail.con>
2018-09-18 10:20:53 -0400

    routinglogin in progress

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
 import { Token } from '../models/Token';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 const Api_Url = 'http://localhost:57751';
 
@@ -16,10 +17,19 @@ export class AuthService {
   register(regUserData: RegisterUser) {
     return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
   }
+
   login(loginInfo){
     const str = 
     `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
-    return this._http.post(`${Api_Url}/token`, str).subscribe( (token: Token) => { localStorage.setItem('id_token', token.access_token);
+    return this._http.post(`${Api_Url}/token`, str).subscribe( (token: Token) => { localStorage.setItem('id_token', token.access_token); this._router.navigate(['/']);
     });
+  }
+
+  currentUser(): Observable<Object> {
+    if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
+
+    const authHeader = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('id_token')}`);
+
+    return this._http.get(`${Api_Url}/api/Account/UserInfo`, { headers: authHeader });
   }
 }
